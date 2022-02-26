7e230a1436b2f433e0909653278b9bb8c5f9462a
Usama-Ishfaq101 <usamasandhu1642@gmail.com>
2022-02-26 18:33:59 +0500

    create user model

---
+++ server/src/models/userModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [(true, "enter first name")],
+    },
+    lastName: {
+      type: String,
+      required: [(true, "enter last name")],
+    },
+    email: {
+      type: String,
+      required: [(true, "enter user email")],
+    },
+    password: {
+      type: String,
+      required: [(true, "enter user password")],
+    },
+    role: {
+      type: String,
+      enum: ["admin", "customer"],
+      default: "customer",
+    },
+    address: {
+      type: String,
+    },
+    phoneNumber: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("users", userSchema);
