3baaf2ef277864bd179f55d17ecd9b8f003684cd
Mudassir742 <mudassirwaheed242@gmail.com>
2022-02-27 19:18:44 +0500

    add middlewares in product

--- server/src/routes/productRoute.js
+++ server/src/routes/productRoute.js
@@ -1,9 +1,20 @@
-const router=require('express').Router();
-const productController=require('../controllers/productController')
+const router = require("express").Router();
+const productController = require("../controllers/productController");
+const { verifyToken, verifyRole } = require("../middlewares/authMiddleware");
 
-router.post("/addproduct",productController.addNewProduct);
-router.put("/updateProduct/:id",productController.updateProduct);
-router.get("/showproduct/:id",productController.showProductById);
-router.get("/showproduct/",productController.showAllProduct);
+router.post(
+  "/addproduct",
+  verifyToken,
+  verifyRole(["admin"]),
+  productController.addNewProduct
+);
+router.put(
+  "/updateProduct/:id",
+  verifyToken,
+  verifyRole(["admin"]),
+  productController.updateProduct
+);
+router.get("/showproduct/:id", productController.showProductById);
+router.get("/showproduct/", productController.showAllProduct);
 
 module.exports = router;
