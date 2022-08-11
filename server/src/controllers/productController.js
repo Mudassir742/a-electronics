8afbc085cd55df657b283a54f72987cdcc72040d
Mudassir742 <mudassirwaheed242@gmail.com>
2022-08-11 23:24:43 +0500

    update status codes for products

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -4,18 +4,16 @@ const Category = require("../models/categoryModel");
 exports.addNewProduct = async (req, res) => {
   try {
     const { categoryId, name, price, description, quantity, model } = req.body;
+
     if (!categoryId || !name || !price || !description || !quantity || !model) {
-      return res.status(422).json({
-        error: "input fields are empty",
-        data: null,
+      return res.status(409).json({
+        error: "bad input",
       });
     }
 
     const category = await Category.findById({ _id: categoryId });
     if (!category) {
-      return res
-        .status(422)
-        .json({ error: "no category found aganist ID", data: null });
+      return res.status(404).json({ error: "no category found" });
     }
 
     const newProduct = await Product.create({
@@ -27,17 +25,15 @@ exports.addNewProduct = async (req, res) => {
       model,
     });
     if (!newProduct) {
-      return res
-        .status(422)
-        .json({ error: "unable to add product", data: null });
+      return res.status(422).json({ error: "unable to add product" });
     }
 
-    return res.status(201).json({ error: null, data: newProduct });
+    return res.status(200).json({ data: newProduct });
   } catch (err) {
     console.log(err.message);
     return res
-      .status(422)
-      .json({ error: "Unexpected error occur", data: null });
+      .status(500)
+      .json({ error: "Unexpected server error while adding product" });
   }
 };
 
@@ -45,43 +41,36 @@ exports.deleteProduct = async (req, res) => {
   try {
     const productId = req.params.id;
     if (!productId) {
-      return res.status(422).json({ error: "Fields are empty", data: null });
+      return res.status(409).json({ error: "bad input" });
     }
-    const isProductDeleted = await Product.deleteOne({ _id: productId });
-    if (isProductDeleted.deletedCount === 0) {
-      return res
-        .status(422)
-        .json({ error: "Unable to delete product", data: null });
+    const isProductDeleted = await Product.findByIdAndDelete({
+      _id: productId,
+    });
+    if (!isProductDeleted) {
+      return res.status(422).json({ error: "Unable to delete product" });
     }
 
-    return res.status(201).json({ error: null, data: isProductDeleted });
+    return res.status(200).json({ data: "product deleted successfully" });
   } catch (err) {
     console.log(err.message);
     return res
-      .status(422)
-      .json({ error: "Unexpected error occur", data: null });
+      .status(500)
+      .json({ error: "Unexpected server error while deleting product" });
   }
 };
 
 exports.updateProduct = async (req, res) => {
   try {
     const productId = req.params.id;
+
     const { categoryId, name, price, description, quantity, model } = req.body;
     if (!categoryId || !name || !price || !description || !quantity || !model) {
-      return res.status(422).json({
-        error: "input fields are empty",
-        data: null,
+      return res.status(409).json({
+        error: "bad input",
       });
     }
 
-    const category = await Category.findById({ _id: categoryId });
-    if (!category) {
-      return res
-        .status(422)
-        .json({ error: "no category found aganist ID", data: null });
-    }
-
-    const product = await Product.updateOne(
+    const isProductUpdated = await Product.findByIdAndUpdate(
       { _id: productId },
       {
         $set: {
@@ -95,19 +84,18 @@ exports.updateProduct = async (req, res) => {
       }
     );
 
-    if (product.modifiedCount === 0) {
+    if (!isProductUpdated) {
       return res.status(422).json({
         error: "Unable to update product",
-        data: null,
       });
     }
 
-    return res.status(201).json({ error: null, data: product });
+    return res.status(200).json({ data: "product updated successfully" });
   } catch (err) {
     console.log(err.message);
     return res
-      .status(422)
-      .json({ error: "Unexpected error occur", data: null });
+      .status(500)
+      .json({ error: "Unexpected server error while updating product" });
   }
 };
 
@@ -115,37 +103,33 @@ exports.showProductById = async (req, res) => {
   try {
     const productId = req.params.id;
     if (!productId) {
-      return res.status(422).json({ error: "Fields are empty", data: null });
+      return res.status(409).json({ error: "bad input" });
     }
 
     const product = await Product.findById({ _id: productId });
+
     if (!product) {
-      return res
-        .status(422)
-        .json({ error: "No product found aganist ID", data: null });
+      return res.status(422).json({ error: "No product found" });
     }
 
-    res.status(201).json({ error: null, data: product });
+    res.status(200).json({ data: product });
   } catch (err) {
     console.log(err.message);
     return res
-      .status(422)
-      .json({ error: "Unexpected error occur", data: null });
+      .status(500)
+      .json({ error: "Unexpected server error while getting product" });
   }
 };
 
 exports.showAllProduct = async (req, res) => {
   try {
     const product = await Product.find({});
-    if (!product) {
-      return res.status(422).json({ error: "No product found", data: null });
-    }
 
-    res.status(201).json({ error: null, data: product });
+    res.status(201).json({ data: product });
   } catch (err) {
     console.log(err.message);
     return res
-      .status(422)
-      .json({ error: "Unexpected error occur", data: null });
+      .status(500)
+      .json({ error: "Unexpected server error while updating product" });
   }
 };
--- server/src/routes/productRoute.js
+++ server/src/routes/productRoute.js
@@ -3,24 +3,24 @@ const productController = require("../controllers/productController");
 const { verifyToken, verifyRole } = require("../middlewares/authMiddleware");
 
 router.post(
-  "/addproduct",
+  "/add-product",
   // verifyToken,
   // verifyRole(["admin"]),
   productController.addNewProduct
 );
 router.put(
-  "/updateProduct/:id",
+  "/update-Product/:id",
   // verifyToken,
   // verifyRole(["admin"]),
   productController.updateProduct
 );
 router.delete(
-  "/deleteproduct/:id",
+  "/delete-product/:id",
   // verifyToken,
   // verifyRole(["admin"]),
   productController.deleteProduct
 );
-router.get("/showproduct/:id", productController.showProductById);
-router.get("/showproduct/", productController.showAllProduct);
+router.get("/show-product/:id", productController.showProductById);
+router.get("/show-product/", productController.showAllProduct);
 
 module.exports = router;
