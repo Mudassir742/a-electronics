fc59cdcca3e9391f513e45dce19cc688fb7f1a8f
Mudassir742 <mudassirwaheed242@gmail.com>
2022-08-17 11:48:28 +0500

    create order model

---
+++ server/src/models/orderModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    customerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "users",
+    },
+    items: {
+      type: Array,
+      ref: "products",
+    },
+    orderStatus: {
+      type: String,
+      enum: ["pending", "placed", "packing", "shipped", "delivered"],
+      default: "pending",
+    },
+    deliveryDate: {
+      type: Date,
+    },
+    amount: {
+      type: "String",
+      required: [true, "order amount is missing"],
+    },
+    ip: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("orders", orderSchema);
